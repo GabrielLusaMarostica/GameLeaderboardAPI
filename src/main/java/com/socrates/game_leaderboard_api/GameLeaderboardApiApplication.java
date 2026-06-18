cc2f2ec5a9ecd69e497889ab0af4f70a7079219d
Gabriel Lusa Marostica <gabriellmarostica@gmail.com>
2026-06-18 13:37:32 -0300

    Initial commit: configuracao base do projeto Spring Boot

---
+++ src/main/java/com/socrates/game_leaderboard_api/GameLeaderboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.socrates.game_leaderboard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLeaderboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLeaderboardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socrates/game_leaderboard_api/GameLeaderboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socrates.game_leaderboard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLeaderboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
